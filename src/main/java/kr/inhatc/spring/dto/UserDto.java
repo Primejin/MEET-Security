a59bc48dcb2f34343d66599444d3c8964e8c65aa
Primejin <sky585456@naver.com>
2021-10-10 02:59:51 +0900

    [#1] feat/BE/security : Add basic spring security
    시큐리티 로그인과 회원가입, OAuth소셜로그인(구글)까지 우선 완료 및 협업을 위한 첫 커밋(BE FE 둘다 된 상태)

--- src/main/java/kr/inhatc/spring/dto/UserDto.java
+++ src/main/java/kr/inhatc/spring/dto/UserDto.java
@@ -12,19 +12,22 @@ public class UserDto {
 	private Long id;
 	private String username;
 	private String password;
+	private String email;
 	
 	public User toEntity() {
 		return User.builder()
 				.id(id)
 				.username(username)
 				.password(password)
+				.email(email)
 				.build();
 	}
 	
 	@Builder
-	public UserDto(Long id, String username, String password ) {
+	public UserDto(Long id, String username, String password, String email ) {
 		this.id = id;
 		this.username = username;
 		this.password = password;
+		this.email = email;
 	}
 }
